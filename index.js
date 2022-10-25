c768820345e9cb08bad3b955d33d0e9e014e8093
Kaiser <ahmedkaiser.001@gmail.com>
2022-10-25 22:06:43 +0600

    added categories json file and added cors

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5001;
 
+app.use(cors());
+
+const categories = require('./data/course_category.json');
 const data = require('./data/course_data.json');
 
 app.get('/courses', (req, res) => {
     res.send(data);
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
 
 app.get('/course/:id', (req, res) => {
     res.send(data.find(item => item.id === req.params.id));
